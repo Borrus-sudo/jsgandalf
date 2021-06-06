a8dc9031f9e5ce5a99388359a2e83c7909447d65
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-06-06 20:39:35 +0530

    Initial commit ✨✨

---
+++ src/components/listBlogs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "listBlogs",
+  props: {
+    msg: String as PropType<string>,
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="hero-btn text-xl font-extrabold">
+    This is home page yeah!!!!!!!!!!!!
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
