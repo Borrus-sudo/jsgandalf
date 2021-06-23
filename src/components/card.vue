31e7133c6280d59b8f14882ab2d0fff358aaa523
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-06-23 23:04:04 +0530

    Improving font-size even more 😖

--- src/components/card.vue
+++ src/components/card.vue
@@ -20,10 +20,10 @@
     <div
       class="
         font-bold
-        text-lg
-        lg:text-2xl
-        xl:text-3xl
-        2xl:text-4xl
+        text-md
+        lg:text-lg
+        xl:text-xl
+        2xl:text-2xl
         underline
         break-words
       "
