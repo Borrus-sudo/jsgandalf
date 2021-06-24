7c68f9f99e7485593dd0215a0f4f4916234b33a3
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-06-24 12:05:01 +0530

    Adding a new background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        blackShade1: "#000000",
-        blackShade2: "#000000d8",
-      },
-      fontFamily: {
-        default: [
-          "Poppins",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          "Segoe UI",
-          "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "sans-serif",
-        ],
-      },
+    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                default: [
+                    "Poppins",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    "Segoe UI",
+                    "Roboto",
+                    "Helvetica Neue",
+                    "Arial",
+                    "sans-serif",
+                ],
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
