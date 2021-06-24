6369f6ca7ffef7b063fb6413c616d975b348ccdb
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-06-24 19:48:42 +0530

    Lint fix

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,4 +1,10 @@
-export type blog = { meta: string; message: string; time: string; star: number; _id: string };
+export type blog = {
+  meta: string;
+  message: string;
+  time: string;
+  star: number;
+  _id: string;
+};
 export interface blogs {
   blogs: Array<blog>;
   start: number;
