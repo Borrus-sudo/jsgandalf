70a79e4d069ea5ca8b8d1c341ef9178bcd3d19f8
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-06-26 11:00:54 +0530

    Adding highlight.js atom one dark theme css file

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./styles/index.css";
-
+import "./styles/hljstheme.css";
 createApp(App).use(router).mount("#app");
